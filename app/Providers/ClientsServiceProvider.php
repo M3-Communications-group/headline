99dfe2680cd9a188b86bf8a09037b0c903e9a976
BakalovM3 <sbakalov@m3bg.com>
2024-03-29 11:53:03 +0200

    .

---
+++ app/Providers/ClientsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClientsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ArticlesServiceProvider::class,
+    App\Providers\ClientsServiceProvider::class,
+    App\Providers\EmailServiceProvider::class,
+    App\Providers\RSSServiceProvider::class,
+    App\Providers\ReportsServiceProvider::class,
+    App\Providers\SettingsServiceProvider::class,
 ];
